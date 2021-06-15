73b7cbab968133d8bd9c5ba3519bdd2259a6c641
Joshua-stdnt <joshua@stdnt.co.za>
2021-06-16 00:34:33 +0200

    first commit

---
+++ plugins/quasar.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import '../assets/sass/quasar-overrides.sass'
+//import 'quasar/dist/quasar.addon.min.css'
+import 'quasar-extras/material-icons.js'
+import 'quasar-extras/material-icons/material-icons.css'
+//import 'quasar/dist/quasar.addon.css'
+//import 'quasar/dist/quasar.addon.min.css'
+//import 'quasar/dist/quasar.addon.rtl.css'
+//import 'quasar/dist/quasar.addon.rtl.min.css'
+//import 'quasar/dist/quasar.css'
+//import 'quasar/dist/quasar.min.css'
+//import 'quasar/dist/quasar.rtl.css'
+//import 'quasar/dist/quasar.rtl.min.css'
+
+import Quasar from 'quasar/dist/quasar.umd.min.js'
+
+Vue.use(Quasar)
+//Vue.use(VueWindowSize)
