1a0bfe494947b016757b7ac58cd9684d183b4e96
Joshua-stdnt <joshua@stdnt.co.za>
2021-06-16 14:54:47 +0200

    50% done with homepage

---
+++ components/SellBuy/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="stdnt-sell-box col-3">
+      <div class="stdnt-sell-box2">
+        <div class="stdnt-sell-title">{{ title }}</div>
+        <div class="stdnt-sell-text">
+          {{ text }}
+        </div>
+        <q-btn rounded outline class="stdnt-sell-find">{{ find }}</q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'text', 'find'],
+}
+</script>
+
+<style lang='sass' scoped>
+.stdnt-sell-box
+  //width: 400px
+  .stdnt-sell-box2
+    //margin-left: 100px
+    width: 400px
+    .stdnt-sell-title
+      margin-top: 161px
+      font-family: Junegull
+      font-style: normal
+      font-weight: normal
+      font-size: 64px
+      line-height: 64px
+    .stdnt-sell-text
+      margin-top: 1px
+      font-family: Helvetica
+      font-style: normal
+      font-weight: normal
+      font-size: 18px
+      line-height: 21px
+      text-align: center
+      letter-spacing: -0.01em
+    .stdnt-sell-find
+      margin-top: 7px
+      font-family: Helvetica
+      font-style: normal
+      font-weight: normal
+      font-size: 13px
+      color: rgba(255, 255, 255, 0.5)
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <q-layout view="hHh lpR fFf">
       <q-page-container>
         <q-page>
+          <!-- Header -->
           <div align="center">
             <div class="header row justify-between items-center">
               <div class="row">
@@ -11,6 +12,7 @@
                   alt="logo"
                   style="margin-right: 5px"
                 />
+
                 <div class="header-logo">STDNT</div>
               </div>
               <div class="row header-component items-center">
@@ -22,11 +24,14 @@
               </div>
             </div>
           </div>
+          <!-- Body -->
           <div align="center">
+            <!-- Title-->
             <div class="stdnt-title">
               A CONVENIENT WAY TO BUY AND SELL TEXTBOOKS
             </div>
-            <div class="stdnt-sell row no-wrap">
+            <!-- Sell section -->
+            <div class="stdnt-sell row no-wrap justify-center">
               <div class="stdnt-sell-box col-3">
                 <div class="stdnt-sell-box2">
                   <div class="stdnt-sell-title">SELL</div>
@@ -46,6 +51,58 @@
                 alt="office"
               />
             </div>
+            <!-- Buy section -->
+            <div class="stdnt-buy row no-wrap justify-center">
+              <img
+                class="stdnt-buy-image col-8"
+                src="~/assets/images/im2.svg"
+                alt="office"
+              />
+              <div class="stdnt-buy-box col-3">
+                <div class="stdnt-buy-box2">
+                  <div class="stdnt-buy-title">BUY</div>
+                  <div class="stdnt-buy-text">
+                    SEARCH FOR A BOOK YOU NEED AND BUY IT. IT’S THAT EASY.
+                  </div>
+                  <q-btn rounded outline class="stdnt-buy-find"
+                    >FIND OUT MORE</q-btn
+                  >
+                </div>
+              </div>
+              <div style="margin-right: 100px">
+                <!--SellBuy :title="title" :text="text" :find="find" /-->
+              </div>
+            </div>
+            <!-- Collect Deliver -->
+            <div class="stdnt-coldel row no-wrap justify-center">
+              <div class="stdnt-coldel-collect">
+                <div class="stdnt-coldel-title">Collect</div>
+                <div class="stdnt-coldel-text">
+                  WE’LL FETCH THE BOOK FROM YOU AND MAKE SURE IT GET’S TO THE
+                  RIGHT PLACE.
+                </div>
+                <img
+                  class="stdnt-coldel-img"
+                  src="~/assets/images/im3.svg"
+                  alt="collect"
+                />
+                <div class="stdnt-coldel-sell">
+                  <div>BUY</div>
+                </div>
+              </div>
+              <div class="stdnt-coldel-collect">
+                <div class="stdnt-coldel-title">deliver</div>
+                <div class="stdnt-coldel-text">
+                  WE’LL BRING YOUR NEW PURCHASE TO YOUR DOOR OR WHEREVER YOU’D
+                  PREFER.
+                </div>
+                <img
+                  class="stdnt-coldel-img"
+                  src="~/assets/images/im4.svg"
+                  alt="deliver"
+                />
+              </div>
+            </div>
           </div>
         </q-page>
       </q-page-container>
@@ -54,7 +111,18 @@
 </template>
 
 <script>
-export default {}
+import SellBuy from '~/components/SellBuy'
+export default {
+  components: { SellBuy },
+  data() {
+    return {
+      title: 'BUY',
+      text: 'SEARCH FOR A BOOK YOU NEED AND BUY IT. IT’S THAT EASY.',
+      find: 'FIND OUT MORE',
+      img: '~/assets/images/im2.svg',
+    }
+  },
+}
 </script>
 
 <style lang='sass' scoped>
@@ -84,9 +152,11 @@ export default {}
   line-height: 85px
   letter-spacing: -0.01em
 .stdnt-sell
-  max-width: 1200px
+  //max-width: 1200px
+  height: 450px
   .stdnt-sell-box
-    //width: 400px
+    width: 300px
+    margin-left: 100px
     .stdnt-sell-box2
       margin-left: 100px
       width: 400px
@@ -115,6 +185,64 @@ export default {}
         color: rgba(255, 255, 255, 0.5)
   .stdnt-sell-image
     margin-right: 100px
-    width: 75%
-    height: 75%
+    width: 800px
+    height: 800px
+.stdnt-buy
+  margin-left: 300px
+  height: 600px
+  .stdnt-buy-box
+    width: 300px
+    margin-right: 300px
+    .stdnt-buy-box2
+      //margin-right: 200px
+      width: 400px
+      .stdnt-buy-title
+        margin-top: 161px
+        font-family: Junegull
+        font-style: normal
+        font-weight: normal
+        font-size: 64px
+        line-height: 64px
+      .stdnt-buy-text
+        margin-top: 1px
+        font-family: Helvetica
+        font-style: normal
+        font-weight: normal
+        font-size: 18px
+        line-height: 21px
+        text-align: center
+        letter-spacing: -0.01em
+      .stdnt-buy-find
+        margin-top: 7px
+        font-family: Helvetica
+        font-style: normal
+        font-weight: normal
+        font-size: 13px
+        color: rgba(255, 255, 255, 0.5)
+  .stdnt-buy-image
+    //margin-right: 100px
+    width: 800px
+    height: 800px
+.stdnt-coldel
+  max-width: 1200px
+  margin-right: 90px
+  .stdnt-coldel-collect
+    .stdnt-coldel-title
+      font-family: Junegull
+      font-style: normal
+      font-weight: normal
+      font-size: 64px
+      line-height: 64px
+    .stdnt-coldel-text
+      margin-bottom: 5px
+      width: 402px
+      font-family: Helvetica
+      font-style: normal
+      font-weight: normal
+      font-size: 18px
+      line-height: 21px
+      text-align: center
+      letter-spacing: -0.01em
+    //margin-right: 50px
+    .stdnt-coldel-img
 </style>
